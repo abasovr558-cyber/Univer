8bfc66a269396d62b4f010e4a168e34f9dea5568
abasovr558-cyber <abasovr558@gmail.com>
2025-10-02 13:03:54 +0300

    Add files via upload

---
+++ dz1.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+
+
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	float x;
+	float y;
+	cout << "введите X  и Y " << endl;
+	cin >> x;
+	cin >> y;
+	if (y == 0) {
+		cout << "Error";
+		return 1;
+	}
+	double pi = 2 * acos(0.0);
+	float R = cos(pi / 4 * x);
+	float S = pow(exp(y), 1.0 / 8.0) * (1.0 / pow(5, y));
+	cout << "C = " << max(R, S) << endl;
+	cin.get();
+	return 0;
+
+
+	
+
+}
+
